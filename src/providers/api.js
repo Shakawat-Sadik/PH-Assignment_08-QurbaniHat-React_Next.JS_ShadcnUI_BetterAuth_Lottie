0e331050e791356ac7a1213d7fcae73bd60e0334
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-05-23 02:10:00 +0600

    Animals page contructing

--- src/providers/api.js
+++ src/providers/api.js
@@ -1,20 +1,26 @@
-// import animalPromise from "/animalsEndpoint.json" // This only works in Vite or Create React App
+// import animalPromise from "/animalsEndpoint.json" // This only works in Vite or Create React App when json is kept in public folder
+import animalPromise from "@/providers/animalsEndpoint.json"
 
 export const animalsFetch = async () => {
   const baseURL = process.env.NEXT_PUBLIC_BASE_URL || process.env.BETTER_AUTH_URL;
+  
   try {
     const res = await fetch("https://api.npoint.io/8982ace3b4fd9eeb22fc");
-    const intRes = await fetch(`${baseURL}/animalsEndpoint.json`);
+    
     if (res.ok) {
       return await res.json();
-    } else if (intRes.ok){
-      console.log("serving data from backup generator");
-      return await intRes.json();
-    } else {
-      throw new Error(`Failed to fetch animals: ${res.status}`);
     }
+    // } else if (intRes.ok){
+    //   console.log("serving data from backup generator");
+    //   return await intRes.json();
+    // } else {
+    //   throw new Error(`Failed to fetch animals: ${res.status}`);
+    // }
   } catch (error) {
     console.log("Failed to fetch animals data", error);
     throw error;
   }
+
+  console.log("Serving data from the backup generator")
+  return animalPromise;
 };
